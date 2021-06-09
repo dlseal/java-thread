6087b2ab720f2474f4f31452cb5e63bc10498ac2
duliang-1 <duliang-1@thunisoft.com>
2021-06-09 09:33:32 +0800

    2021/6/8 阅读笔记-CyclicBarrier 使用ReentrantLock+Condition实现

--- src/java/util/concurrent/CyclicBarrier.java
+++ src/java/util/concurrent/CyclicBarrier.java
@@ -37,7 +37,9 @@ package java.util.concurrent;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-/**
+/**一种同步辅助工具，它允许一组线程全部等待彼此到达公共屏障点。
+ *  CyclicBarriers 在涉及固定大小的线程组的程序中很有用，这些线程必须偶尔相互等待。
+ *  屏障被称为<em>循环<em>，因为它可以在等待线程被释放后重新使用。
  * A synchronization aid that allows a set of threads to all wait for
  * each other to reach a common barrier point.  CyclicBarriers are
  * useful in programs involving a fixed sized party of threads that
@@ -137,7 +139,9 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Doug Lea
  */
 public class CyclicBarrier {
-    /**
+    /**屏障的每次使用都表示为一个生成实例。每当屏障被触发或重置时，生成都会发生变化。
+     * 可以有许多代与使用屏障的线程相关联 - 由于锁可能以不确定的方式分配给等待线程 -
+     * 但一次只能激活其中之一（{@code count} 适用的那个） ) 其余的要么坏了要么被绊倒了。
      * Each use of the barrier is represented as a generation instance.
      * The generation changes whenever the barrier is tripped, or
      * is reset. There can be many generations associated with threads
@@ -154,6 +158,7 @@ public class CyclicBarrier {
 
     /** The lock for guarding barrier entry */
     private final ReentrantLock lock = new ReentrantLock();
+    // 到达屏障的等待条件
     /** Condition to wait on until tripped */
     private final Condition trip = lock.newCondition();
     /** The number of parties */
@@ -163,17 +168,18 @@ public class CyclicBarrier {
     /** The current generation */
     private Generation generation = new Generation();
 
-    /**
+    /**仍在等待的线程数量。每一代从parties倒计时到 0。它会在每一代或损坏时重置为parties。
      * Number of parties still waiting. Counts down from parties to 0
      * on each generation.  It is reset to parties on each new
      * generation or when broken.
      */
     private int count;
 
-    /**
+    /**更新屏障的状态并唤醒每个人。仅在持有锁时调用。
      * Updates state on barrier trip and wakes up everyone.
      * Called only while holding lock.
      */
+    // 更新屏障，开启新的一轮
     private void nextGeneration() {
         // signal completion of last generation
         trip.signalAll();
@@ -182,13 +188,16 @@ public class CyclicBarrier {
         generation = new Generation();
     }
 
-    /**
+    /**当线程所有线程到达屏障点，打破当前屏障,（应该是最后一轮执行）
      * Sets current barrier generation as broken and wakes up everyone.
      * Called only while holding lock.
      */
     private void breakBarrier() {
+        // 所有线程以到达屏障点
         generation.broken = true;
+        // 重置线程数量
         count = parties;
+        // 唤醒所有等待队列中的线程
         trip.signalAll();
     }
 
@@ -203,18 +212,22 @@ public class CyclicBarrier {
         try {
             final Generation g = generation;
 
+            // 屏障已经打破，抛出异常
             if (g.broken)
                 throw new BrokenBarrierException();
 
+            // 当前线程已经被其他线程中断，打破屏障，抛出异常
             if (Thread.interrupted()) {
                 breakBarrier();
                 throw new InterruptedException();
             }
 
             int index = --count;
+            // 如果所有线程都到达了屏障点
             if (index == 0) {  // tripped
                 boolean ranAction = false;
                 try {
+                    // 执行屏障的任务
                     final Runnable command = barrierCommand;
                     if (command != null)
                         command.run();
@@ -222,23 +235,29 @@ public class CyclicBarrier {
                     nextGeneration();
                     return 0;
                 } finally {
+                    // TODO 这里要怎么才能进来？
                     if (!ranAction)
                         breakBarrier();
                 }
             }
 
+            // 循环，直到所有线程到达屏障点、中断或者超时
             // loop until tripped, broken, interrupted, or timed out
             for (;;) {
                 try {
                     if (!timed)
+                        // 当前线程进入等待队列
                         trip.await();
                     else if (nanos > 0L)
+                        // 有时限的进入等待队列
                         nanos = trip.awaitNanos(nanos);
                 } catch (InterruptedException ie) {
+                    // 没有开启新屏障，并且当前屏障尚未打破，其他线程中断了当前线程
                     if (g == generation && ! g.broken) {
                         breakBarrier();
                         throw ie;
                     } else {
+                        // 即使我们没有被中断，我们也即将完成等待，因此该中断被视为“属于”后续执行。
                         // We're about to finish waiting even if we had not
                         // been interrupted, so this interrupt is deemed to
                         // "belong" to subsequent execution.
@@ -262,12 +281,14 @@ public class CyclicBarrier {
         }
     }
 
-    /**
+    /**创建一个新的 {@code CyclicBarrier}，当给定数量的参与方（线程）正在等待它时，
+     * 它将触发，并在屏障被触发时执行给定的屏障操作，由进入屏障的最后一个线程执行
      * Creates a new {@code CyclicBarrier} that will trip when the
      * given number of parties (threads) are waiting upon it, and which
      * will execute the given barrier action when the barrier is tripped,
      * performed by the last thread entering the barrier.
      *
+     * 必须到达屏障的线程数
      * @param parties the number of threads that must invoke {@link #await}
      *        before the barrier is tripped
      * @param barrierAction the command to execute when the barrier is
@@ -453,7 +474,9 @@ public class CyclicBarrier {
         }
     }
 
-    /**
+    /**将障碍重置为其初始状态。如果任何一方当前正在屏障处等待，他们将返回 {@link BrokenBarrierException}。
+     * 请注意，在由于其他原因发生损坏后 <em><em> 进行重置可能会很复杂；
+     * 线程需要以其他方式重新同步，并选择一个来执行重置。最好为后续使用创建一个新的屏障。
      * Resets the barrier to its initial state.  If any parties are
      * currently waiting at the barrier, they will return with a
      * {@link BrokenBarrierException}. Note that resets <em>after</em>
@@ -466,7 +489,9 @@ public class CyclicBarrier {
         final ReentrantLock lock = this.lock;
         lock.lock();
         try {
+            // 打破当前屏障
             breakBarrier();   // break the current generation
+            // 开启新屏障
             nextGeneration(); // start a new generation
         } finally {
             lock.unlock();
